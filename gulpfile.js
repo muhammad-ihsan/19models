501cc942b320b1b4123e6d8e309fac2afe8ebd73
Muhammad Ihsan <muhammad-ihsan@outlook.com>
2018-01-23 22:08:00 +0700

    update package and gulpfile js

--- gulpfile.js
+++ gulpfile.js
@@ -14,6 +14,8 @@ const plugins = [
   './node_modules/jquery/dist/jquery.js',
   './node_modules/bootstrap3/dist/js/bootstrap.js',
   './node_modules/mustache/mustache.js',
+  './node_modules/bootstrap-datepicker/js/bootstrap-datepicker.js',
+  './node_modules/moment/src/moment.js',
   'views/scripts/material-kit.js',
 ]
 
